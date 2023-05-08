03af89b9da60b1ac1b95b35a057a3f1b3c5f0ced
DaraKolm <75583812+DaraKolm@users.noreply.github.com>
2023-05-08 23:00:17 +0300

    lab 1 fixed lable of activity

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -1,15 +1,79 @@
 package com.example.android_lab;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText userText;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private TextView textView;
+    private String text;
+    private String fontSize;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button butOK = findViewById(R.id.buttonOK);
+        Button butCancel = findViewById(R.id.buttonCancel);
+        userText = findViewById(R.id.userText);
+        textView = findViewById(R.id.textView3);
+        radioGroup = findViewById(R.id.radioGroup1);
+        radioGroup.check(findViewById(R.id.radioButton1).getId());
+
+        butOK.setOnClickListener(v -> {
+            text = userText.getText().toString();
+            if(text.trim().equals("")){
+                Toast.makeText(MainActivity.this, R.string.message_empty,Toast.LENGTH_LONG).show();
+            }else{
+
+                 int radioButtonID;
+                 /*radioButtonID=0;
+                boolean flag = true;
+                try{radioButtonID = radioGroup.getCheckedRadioButtonId();}
+
+                    catch(Exception e){
+                        Toast.makeText(MainActivity.this, R.string.message_empty_radio,Toast.LENGTH_LONG).show();
+                        flag=false;
+                }
+                if(flag){
+                    //Toast.makeText(MainActivity.this, R.string.message_empty_radio,Toast.LENGTH_LONG).show();
+                    //Toast.makeText(MainActivity.this, "rdID"+radioButtonID,Toast.LENGTH_LONG).show();
+                    radioButton = radioGroup.findViewById(radioButtonID);
+                    fontSize = radioButton.getText().toString().replace("sp","");
+                    textView.setText(text);
+                    textView.setTextSize(Float.parseFloat(fontSize));
+
+                }*/
+                radioButtonID = radioGroup.getCheckedRadioButtonId();
+                radioButton = radioGroup.findViewById(radioButtonID);
+                fontSize = radioButton.getText().toString().replace("sp","");
+                textView.setText(text);
+                textView.setTextSize(Float.parseFloat(fontSize));
+            }
+        });
+
+        butCancel.setOnClickListener(v -> {
+            userText.setText(R.string.text_ent);
+            textView.setText(R.string.text_ent);
+            textView.setTextSize(18);
+            //radioGroup.clearCheck();
+            radioGroup.check(findViewById(R.id.radioButton1).getId());
+            Toast.makeText(MainActivity.this, R.string.message_cancel,Toast.LENGTH_LONG).show();
+
+        });
+
 
     }
 }
