39495bffeb782e4808622727a66ad36ca015289d
DaraKolm <75583812+DaraKolm@users.noreply.github.com>
2023-05-11 22:17:56 +0300

    horizontal layout and fragment 1 xml(?)

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -19,51 +19,26 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private String text;
     private String fontSize;
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        Button butOK = findViewById(R.id.buttonOK);
-        Button butCancel = findViewById(R.id.buttonCancel);
-        userText = findViewById(R.id.userText);
-        textView = findViewById(R.id.textView3);
-        radioGroup = findViewById(R.id.radioGroup1);
-        radioGroup.check(findViewById(R.id.radioButton1).getId());
-
-        butOK.setOnClickListener(v -> {
+    public View.OnClickListener buttonOK_click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
             text = userText.getText().toString();
             if(text.trim().equals("")/*||text.trim().equals(getString(R.string.text_ent).trim())*/){
                 Toast.makeText(MainActivity.this, R.string.message_empty,Toast.LENGTH_LONG).show();
             }else{
-                 int radioButtonID;
-                 /*radioButtonID=0;
-                boolean flag = true;
-                try{radioButtonID = radioGroup.getCheckedRadioButtonId();}
-
-                    catch(Exception e){
-                        Toast.makeText(MainActivity.this, R.string.message_empty_radio,Toast.LENGTH_LONG).show();
-                        flag=false;
-                }
-                if(flag){
-                    //Toast.makeText(MainActivity.this, R.string.message_empty_radio,Toast.LENGTH_LONG).show();
-                    //Toast.makeText(MainActivity.this, "rdID"+radioButtonID,Toast.LENGTH_LONG).show();
-                    radioButton = radioGroup.findViewById(radioButtonID);
-                    fontSize = radioButton.getText().toString().replace("sp","");
-                    textView.setText(text);
-                    textView.setTextSize(Float.parseFloat(fontSize));
-
-                }*/
+                int radioButtonID;
                 radioButtonID = radioGroup.getCheckedRadioButtonId();
                 radioButton = radioGroup.findViewById(radioButtonID);
                 fontSize = radioButton.getText().toString().replace("sp","");
                 textView.setText(text);
                 textView.setTextSize(Float.parseFloat(fontSize));
             }
-        });
+        }
+    };
+    public View.OnClickListener buttonCancel_click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-        butCancel.setOnClickListener(v -> {
             //userText.setText(R.string.text_ent);
             userText.setText("");
             textView.setText(R.string.text_ent);
@@ -72,7 +47,23 @@ public class MainActivity extends AppCompatActivity {
             radioGroup.check(findViewById(R.id.radioButton1).getId());
             Toast.makeText(MainActivity.this, R.string.message_cancel,Toast.LENGTH_LONG).show();
 
-        });
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button buttonOK = findViewById(R.id.buttonOK);
+        final Button buttonCancel = findViewById(R.id.buttonCancel);
+        userText = findViewById(R.id.userText);
+        textView = findViewById(R.id.textView3);
+        radioGroup = findViewById(R.id.radioGroup1);
+        radioGroup.check(findViewById(R.id.radioButton1).getId());
+
+        buttonOK.setOnClickListener(buttonOK_click);
+        buttonCancel.setOnClickListener(buttonCancel_click);
 
 
     }
